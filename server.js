cb6683ad5671d6f53275bdb431b26ff4cacab518
krispyrendang <taswinder.s@gmail.com>
2023-10-01 12:27:12 -0700

    Moved server.js to root

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+const port = 8080;
+
+const utils = require('./COMP4537/labs/3/modules/utils');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let q = url.parse(req.url, true);
+    if(q.pathname == "/") {
+        res.end("This is the root.");
+    }
+    else if(q.pathname == "/COMP4537/labs/3/getDate/") {
+        let text = utils.getDate(q.query["name"]);
+        res.end(`<div style=color:blue>` + text + `<div>/`);
+    }
+}).listen(port);
+
+
+console.log('Server is running and listening');
