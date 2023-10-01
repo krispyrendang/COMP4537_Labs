86b45b4ab2e5338cb504444839d3eb147f542151
krispyrendang <taswinder.s@gmail.com>
2023-10-01 12:33:02 -0700

    removed routing for '/' path

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/html'});
 
     let q = url.parse(req.url, true);
-    if(q.pathname == "/") {
-        res.end("This is the root.");
-    }
-    else if(q.pathname == "/COMP4537/labs/3/getDate/") {
+    // if(q.pathname == "/") {
+    //     res.end("This is the root.");
+    // }
+    // else 
+    if(q.pathname == "/COMP4537/labs/3/getDate/") {
         let text = utils.getDate(q.query["name"]);
         res.end(`<div style=color:blue>` + text + `<div>/`);
     }
