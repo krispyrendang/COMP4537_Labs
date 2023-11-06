805263f5d71270e5e83df016bc08e89318445c61
Winston <winston_nguyenn@hotmail.com>
2023-11-05 16:43:55 -0800

    Added params for languages

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
